da26f231b392e1ed6b88d7a0b3d46c6971747628
soyoon26 <barcardi26@gmail.com>
2024-06-09 01:47:47 +0900

    set metaTag

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,21 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="윤플릭스 로고" />
       <a href="https://www.youtube.com/watch?v=ig7ha2zpLZI">
         <div className="App-main"></div>
       </a>
 
       <div className="App-content">
-        <div className="Content-title">Yoonflix 인기 콘텐츠</div>
+        <div className="Content-title" aria-label="윤플릭스 인기 콘텐츠">
+          Yoonflix 인기 콘텐츠
+        </div>
         <Popular />
       </div>
       <div className="App-content">
-        <div className="Content-title">지금 뜨는 콘텐츠</div>
+        <div className="Content-title" aria-label="지금 뜨는 콘텐츠">
+          지금 뜨는 콘텐츠
+        </div>
         <TopRank />
       </div>
       <Footer />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,10 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="Footer-wrap">
-      <div className="Footer">By ISANG</div>;
+      <div className="Footer" aria-label="만든 사람 개발자 ISANG">
+        By ISANG
+      </div>
+      ;
     </div>
   );
 }
