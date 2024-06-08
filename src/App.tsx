f2eaf917859f8b32b1a9d136652cb2e5ff7b9437
soyoon26 <barcardi26@gmail.com>
2024-06-08 23:27:27 +0900

    add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import logo from "./yoon_logo.png";
-import main from "./누가파스타를훔쳐먹었는가.png";
 import "./App.css";
 import Popular from "./components/Popular";
 import TopRank from "./components/TopRank";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <div className="App-main"></div>
-      <header className="App-header">
-        <div>Yoonflix 인기 콘텐츠</div>
+
+      <div className="App-content">
+        <div className="Content-title">Yoonflix 인기 콘텐츠</div>
         <Popular />
-        <div>지금 뜨는 콘텐츠</div>
+      </div>
+      <div className="App-content">
+        <div className="Content-title">지금 뜨는 콘텐츠</div>
         <TopRank />
-      </header>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -13,11 +13,20 @@ interface MovieListProps {
 
 const MovieList: React.FC<MovieListProps> = ({ movies }) => {
   const settings = {
-    dots: true,
-    infinite: true,
+    dots: false,
+    infinite: false,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768, // 모바일 기준 크기
+        settings: {
+          slidesToShow: 3, // 모바일에서는 3개의 슬라이드 표시
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
